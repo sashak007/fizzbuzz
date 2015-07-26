b2c765e5a3960f63f0f08ce196dd6a249c89b8a2
Natasha <natasha.sha.shaaa@gmail.com>
2015-07-26 14:45:32 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var fizzBuzz = function(){
+	
+	for(var i = 1; i <= 1000; i++){
+		
+		if(i%3 && i%5 === 0){
+
+			document.write('FizzBuzz<br>');
+
+		} else if(i%3 === 0){
+
+			document.write('Fizz<br>');
+
+		} else if(i%5 === 0){
+
+			document.write('Buzz<br>');
+
+		} else{
+			document.write(i + '<br>');
+		}
+	
+	}
+
+};
